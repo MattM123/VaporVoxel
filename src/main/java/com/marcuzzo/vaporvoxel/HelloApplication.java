04ee06b66d6a017ae569095c55f48adffe596f12
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-10-31 19:00:04 -0500

    pre-camera centered world rot

--- src/main/java/com/marcuzzo/vaporvoxel/HelloApplication.java
+++ src/main/java/com/marcuzzo/vaporvoxel/HelloApplication.java
@@ -6,110 +6,121 @@ import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.transform.Rotate;
-import javafx.stage.Screen;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class HelloApplication extends Application {
-    private Group group;
+    private Group world;
     private Scene scene;
-    private double oldX;
-    private double oldY;
-
     private double newX;
     private double newY;
-
     private double dx;
     private double dy;
 
-    private Rotate yaw;
-    private Rotate pitch;
     /**
-     * How much to increment or decrement the camera position every time the mouse is moved
+     * How much to increment or decrement the camera rotation every time the mouse is moved
      */
-    private final double mouseSensitivity = 0.03;
+    private final double mouseSensitivity = 0.005;
     /**
      * How much to increment or decrement the camera position every time a directional key is pressed
      */
-    private final double cameraSensitivity = 0.03;
-    private double camX;
-    private double camZ;
+    private final double cameraSensitivity = 0.01;
+    private double camX = 0.0;
+    private double camZ = 0.0;
     private final BooleanProperty w = new SimpleBooleanProperty(false);
     private final BooleanProperty a = new SimpleBooleanProperty(false);
     private final BooleanProperty s = new SimpleBooleanProperty(false);
     private final BooleanProperty d = new SimpleBooleanProperty(false);
     private final BooleanBinding anyPressed = w.or(a).or(s).or(d);
     private boolean pause = false;
-    private double taskbarHeight = Toolkit.getDefaultToolkit().getScreenSize().height
-            - GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getHeight();
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Parent root = fxmlLoader.load();
-        group = new Group();
-        scene = new Scene(group, Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
-                Toolkit.getDefaultToolkit().getScreenSize().getHeight() - taskbarHeight);
-        stage.setTitle("Test");
+        world = new Group(root);
 
-
+        scene = new Scene(world, Double.MAX_VALUE, Double.MAX_VALUE);
+        stage.setTitle("VaporVoxel");
         stage.setScene(scene);
         stage.show();
-
-        yaw = new Rotate(0, Rotate.Y_AXIS);
-        pitch = new Rotate(0, Rotate.X_AXIS);
 
         Box box = new Box(5, 5, 5);
         box.setMaterial(new PhongMaterial(Color.BLUE));
         box.setTranslateZ(40);
-
-        group.getChildren().add(box);
+        world.getChildren().add(box);
 
         PerspectiveCamera camera = new PerspectiveCamera(true);
-        camera.getTransforms().addAll(yaw, pitch);
+        camera.setFarClip(2000);
+        camera.setNearClip(1);
         scene.setCamera(camera);
+        scene.setCursor(Cursor.NONE);
+        Rotate worldRotX = new Rotate(0, Rotate.X_AXIS);
+        Rotate worldRotY = new Rotate(0, Rotate.Y_AXIS);
 
+        Translate worldTransX = new Translate();
+        world.getTransforms().addAll(worldRotY, worldRotX, worldTransX);
         /*===========================================
         Controls camera movement and rotation
         ===========================================*/
+
         AtomicBoolean pressed = new AtomicBoolean(false);
 
-        scene.setOnMousePressed((MouseEvent event) -> {
+        scene.setOnMouseEntered((MouseEvent event) -> {
             pressed.set(true);
             newX = event.getSceneX();
             newY = event.getSceneY();
         });
 
         scene.setOnMouseMoved((MouseEvent event) -> {
+          //  worldRotX.setPivotX(camera.getTranslateX());
+          //  worldRotX.setPivotY(camera.getTranslateY());
+          //  worldRotX.setPivotZ(camera.getTranslateZ());
+
+         //   worldRotY.setPivotX(camera.getTranslateX());
+         //   worldRotY.setPivotY(camera.getTranslateY());
+         //   worldRotY.setPivotZ(camera.getTranslateZ());
+          //  camera.setTranslateX(world.getTranslateX());
+         //   camera.setTranslateY(world.getTranslateY());
+          //  camera.setTranslateZ(world.getTranslateZ());
+
             if (pressed.get()) {
-                oldX = newX;
-                oldY = newY;
+                dx = event.getSceneX() - newX;
+                dy = event.getSceneY() - newY;
+
+                //Left
+                if (dx < 0) {
+                    worldRotY.setAngle(worldRotY.getAngle() - dx);
+                }
+                //Right
+                else if (dx > 0) {
+                    worldRotY.setAngle(worldRotY.getAngle() - dx);
+                }
+                //Up
+                else if (dy < 0) {
+                    worldRotX.setAngle(worldRotX.getAngle() + dy);
+                }
+                //Down
+                else if (dy > 0) {
+                    worldRotX.setAngle(worldRotX.getAngle() + dy);
+                }
                 newX = event.getSceneX();
                 newY = event.getSceneY();
-                dx = newX - oldX;
-                dy = newY - oldY;
-
-                camera.setTranslateX(camera.getTranslateX() - dx * mouseSensitivity);
-                camera.setTranslateY(camera.getTranslateY() - dy * mouseSensitivity);
             }
         });
-        camX = 0.0;
-        camZ = 0.0;
 
         scene.setOnKeyPressed(event -> {
+            System.out.println(camera.getBoundsInParent().getCenterX() + " " + camera.getBoundsInParent().getCenterY()
+                    + " " + camera.getBoundsInParent().getCenterZ());
             switch (event.getCode()){
                 case A -> a.set(true);
                 case D -> d.set(true);
@@ -130,43 +141,43 @@ public class HelloApplication extends Application {
                 }
             }
         });
-
         /*
          Since the KeyPressed event only outputs the latest key event and not concurrent key events
          the event triggers are only used to set booleans to true on key pressed events
          and false on key released events. This timer and boolean binding are responsible for
          doing camera movements at the same time for concurrent key presses.
          */
+
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long timestamp) {
                 if (w.get()) {
                     camZ += cameraSensitivity;
                     if (camZ > 0)
-                        camera.setTranslateZ(camera.getTranslateZ() + camZ);
+                        world.setTranslateZ(world.getTranslateZ() - camZ);
                     else
-                        camera.setTranslateZ(camera.getTranslateZ() - camZ);
+                        world.setTranslateZ(world.getTranslateZ() + camZ);
                 }
                 if (a.get()) {
-                    camX += cameraSensitivity;
+                    camX -= cameraSensitivity;
                     if (camX > 0)
-                        camera.setTranslateX(camera.getTranslateX() + camX);
+                        world.setTranslateX(world.getTranslateX() + camX);
                     else
-                        camera.setTranslateX(camera.getTranslateX() - camX);
+                        world.setTranslateX(world.getTranslateX() - camX);
                 }
                 if (s.get()) {
                     camZ -= cameraSensitivity;
                     if (camZ > 0)
-                        camera.setTranslateZ(camera.getTranslateZ() - camZ);
+                        world.setTranslateZ(world.getTranslateZ() + camZ);
                     else
-                        camera.setTranslateZ(camera.getTranslateZ() + camZ);
+                        world.setTranslateZ(world.getTranslateZ() - camZ);
                 }
                 if (d.get()) {
-                    camX -= cameraSensitivity;
+                    camX += cameraSensitivity;
                     if (camX > 0)
-                        camera.setTranslateX(camera.getTranslateX() - camX);
+                        world.setTranslateX(world.getTranslateX() - camX);
                     else
-                        camera.setTranslateX(camera.getTranslateX() + camX);
+                        world.setTranslateX(world.getTranslateX() + camX);
                 }
             }
         };
